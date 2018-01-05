3feae567f76a0c1fdcebc73153ad56ace65eba78
Arianit Hetemi <nitihetemi@gmail.com>
2018-01-05 01:25:14 +0100

    small fix

--- models/Product.js
+++ models/Product.js
@@ -3,7 +3,7 @@ var mongoose = require('mongoose');
 var ProductSchema = new mongoose.Schema({
   product_name: String,
   product_description: String,
-  production_price: Number,
+  product_price: Number,
   updated_at: { type: Date, default: Date.now },
 });
 
